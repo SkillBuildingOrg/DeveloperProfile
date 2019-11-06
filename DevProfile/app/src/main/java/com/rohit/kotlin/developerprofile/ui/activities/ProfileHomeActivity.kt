56794e7b67880455aebad9898aa2a1e3edc08836
Rohit Joshi <rohit.joshi@tieto.com>
2019-11-06 11:16:37 +0530

    Added layout for home page

--- DevProfile/app/src/main/java/com/rohit/kotlin/developerprofile/ui/activities/ProfileHomeActivity.kt
+++ DevProfile/app/src/main/java/com/rohit/kotlin/developerprofile/ui/activities/ProfileHomeActivity.kt
@@ -2,13 +2,17 @@ package com.rohit.kotlin.developerprofile.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.util.Linkify
 import com.rohit.kotlin.developerprofile.R
+import me.saket.bettermovementmethod.BetterLinkMovementMethod
 
 class ProfileHomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile_home)
         supportActionBar!!.title = "Profile"
+
+        BetterLinkMovementMethod.linkify(Linkify.ALL, this)
     }
 }
---
+++ DevProfile/app/src/main/java/com/rohit/kotlin/developerprofile/ui/fragments/AboutFragment.kt
@@ -0,0 +1,29 @@
+package com.rohit.kotlin.developerprofile.ui.fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.rohit.kotlin.developerprofile.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [AboutFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [AboutFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AboutFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+}
